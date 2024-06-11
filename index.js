42b794acfba4b9d3d0d1e3d03e05817e478aed02
Dominic Mulinda <mulindarweshumba@gmail.com>
2024-06-11 18:45:37 +0300

    TCW complete objects lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "John Doe",
+    streetAddress: "Kwazulu Natal"
+}
+
+function updateEmployeeWithKeyAndValue(object, key, value) {
+  return {
+    ...object,
+    [key]: value,
+  }
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    const newEmployee = employee
+    newEmployee[key] = value;
+    return newEmployee
+}
+
+function deleteFromEmployeeByKey(obj1, key) {
+  const newObj = { ...obj1}
+  delete newObj[key]
+  return newObj
+}
+
+function destructivelyDeleteFromEmployeeByKey (obj, key) {
+    delete obj[key]
+    return obj
+}
