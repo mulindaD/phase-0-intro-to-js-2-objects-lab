d538522d1bdd35cc6f6ca8812eb9948b73ddcd40
Dominic Mulinda <mulindarweshumba@gmail.com>
2024-07-03 02:10:23 +0300

    Refactors code with concise and arrown functions

--- index.js
+++ index.js
@@ -5,26 +5,46 @@ const employee = {
     streetAddress: "Kwazulu Natal"
 }
 
-function updateEmployeeWithKeyAndValue(object, key, value) {
+// function updateEmployeeWithKeyAndValue(object, key, value) {
+//   return {
+//     ...object,
+//     [key]: value,
+//   }
+// }
+const updateEmployeeWithKeyAndValue = (object, key, value) => {
   return {
     ...object,
     [key]: value,
   }
 }
 
-function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
-    const newEmployee = employee
-    newEmployee[key] = value;
-    return newEmployee
+// function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+//     obj[key] = value;
+//     return obj
+// }
+
+const destructivelyUpdateEmployeeWithKeyAndValue = (obj, key, value) => {
+    obj[key] = value;
+    return obj 
 }
 
-function deleteFromEmployeeByKey(obj1, key) {
-  const newObj = { ...obj1}
+const deleteFromEmployeeByKey = (obj, key) => {
+  const newObj = {...obj}
   delete newObj[key]
   return newObj
 }
+// function deleteFromEmployeeByKey(obj1, key) {
+//   const newObj = { ...obj1}
+//   delete newObj[key]
+//   return newObj
+// }
 
-function destructivelyDeleteFromEmployeeByKey (obj, key) {
-    delete obj[key]
-    return obj
+// function destructivelyDeleteFromEmployeeByKey (obj, key) {
+//     delete obj[key]
+//     return obj
+// }
+
+const destructivelyDeleteFromEmployeeByKey = (obj, key) => {
+  delete obj[key]
+  return obj
 }
